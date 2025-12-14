adc8397ffe9f1e78a45716eaf094014c507607d5
Muhammad Maaz <mmaaz.2412182@gmail.com>
2025-12-14 23:30:25 +0500

    Fixed error auto play next song

--- lib/controller/audio_controller.dart
+++ lib/controller/audio_controller.dart
@@ -27,18 +27,25 @@ class AudioController {
   // **********************************************************************
   // SETUP PLAYER STREAM LISTENERS
   // **********************************************************************
+  // **********************************************************************
+  // SETUP PLAYER STREAM LISTENERS
+  // **********************************************************************
   void _setupAudioPlayer() {
     audioPlayer.playerStateStream.listen((playerState) {
       isPlaying.value = playerState.playing;
 
       // Auto play next song when current finishes
       if (playerState.processingState == ProcessingState.completed) {
-        nextSong();
+        // Yahan check karein ke kya hum already last song par tou nahi hain
+        // Taa ke unnecessary next calls na hon
+        if (currentIndex.value < songs.value.length - 1) {
+          nextSong();
+        } else {
+          // Agar playlist khatam ho gayi hai
+          isPlaying.value = false;
+          audioPlayer.stop();
+        }
       }
-    });
-
-    audioPlayer.positionStream.listen((_) {
-      // updates UI progress bar
     });
   }
 
@@ -76,7 +83,7 @@ class AudioController {
   }
 
   // **********************************************************************
-  // PLAY SONG
+  // PLAY SONG (FIXED)
   // **********************************************************************
   Future<void> playSong(int index) async {
     if (index < 0 || index >= songs.value.length) return;
@@ -85,17 +92,28 @@ class AudioController {
       currentIndex.value = index;
       final song = songs.value[index];
 
-      await audioPlayer.stop();
+      // ERROR FIX: Ye line hata dein, iski waja se interruption error ata hai
+      // await audioPlayer.stop();  <-- COMMENTED OUT / REMOVED
 
       // FIX: Local files require Uri.file(...) instead of Uri.parse(...)
       final uri = _buildUri(song.uri);
 
+      // Try setting source
       await audioPlayer.setAudioSource(AudioSource.uri(uri), preload: true);
+
+      //---------------------------------------------------------------------
 
       await audioPlayer.play();
       isPlaying.value = true;
     } catch (e) {
-      print("Error While Playing Song : $e");
+      // Agar error PlayerInterruptedException hai to usay ignore karein
+      // kyunke iska matlab hai naya song load hona shuru ho gaya hai.
+      if (e.toString().contains("PlayerInterruptedException") ||
+          e.toString().contains("aborted")) {
+        print("Loading interrupted by new request (Normal behavior)");
+      } else {
+        print("Error While Playing Song : $e");
+      }
     }
   }
 
